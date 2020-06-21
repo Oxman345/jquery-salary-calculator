95e138a473d7a07f9fb30f005a8a01352b65f536
Amir Mussa <amir.mussa99@gmail.com>
2020-06-20 23:03:10 -0500

    added a dynamic form

--- salaryCalc.js
+++ salaryCalc.js
@@ -1,9 +1,15 @@
 $(document).ready(function(){
-    $('#submitButton').click(function(){
+    $('#submitButton').click(function(event){
+        event.preventDefault()
         let firstName = $('#firstName').val()
-        let LastName = $('#lastName').val()
+        let lastName = $('#lastName').val()
         let id = $('#id').val()
         let title = $('#title').val()
         let annualSalary = $('#annualSalary').val()
+        let results = "<tr><td>" + firstName + "</td><td>" 
+        + lastName + "</td><td>" + id + "</td><td>"
+        + title + "</td><td>" + annualSalary + "</td><td>" 
+        + "<input type='submit' id='delete' value='delete'></td></tr>"
+        $("table tbody").append(results); 
     }) 
 });
