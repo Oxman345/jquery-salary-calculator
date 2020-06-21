8ea31e3867a09e47354437d3c0b1fdf82f5305a0
Amir Mussa <amir.mussa99@gmail.com>
2020-06-20 22:05:26 -0500

    worked on html and css file

---
+++ salaryCalc.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $('#submitButton').click(function(){
+        let firstName = $('#firstName').val()
+        let LastName = $('#lastName').val()
+        let id = $('#id').val()
+        let title = $('#title').val()
+        let annualSalary = $('#annualSalary').val()
+    }) 
+});
